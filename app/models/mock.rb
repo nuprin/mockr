eeefdb4a16a654b0ffbe83230e1447861e05f0a1
Kristjan <kristjan@causes.com>
2008-10-15 16:19:22 -0700

    Teach Mock its next/prev

--- app/models/mock.rb
+++ app/models/mock.rb
@@ -1,19 +1,64 @@
 class Mock < ActiveRecord::Base
+  MOCK_PATH = "public/images/mocks"
+
   class MockDoesNotExist < StandardError
     attr_reader :path
     def initialize(path)
       @path = path
     end
+    def to_s
+      @path
+    end
   end
 
   def self.for(path)
-    unless File.exist?("public/images/mocks/#{path}")
-      raise MockDoesNotExist.new(path)
+    full_path = "#{MOCK_PATH}/#{path}"
+    unless File.exist?(full_path)
+      raise MockDoesNotExist.new(full_path)
     end
     Mock.find_by_path(path) || Mock.create(:path => path)
   end
 
   def image_path
     "mocks/#{path}"
+  end
+
+  def dir
+    path.split('/')[0...-1].join('/')
+  end
+
+  def filename
+    path.split('/').last
+  end
+
+  def next
+    my_index = ordered_family.index(self)
+    ordered_family[my_index+1]
+  end
+
+  def prev
+    my_index = ordered_family.index(self)
+    return nil if my_index == 0
+    ordered_family[my_index-1]
+  end
+
+  def family_filenames
+    @family_filenames ||= Dir.glob("#{MOCK_PATH}/#{dir}/*").map do |path|
+      path.split('/')[3..-1].join('/')
+    end
+  end
+
+  def <=>(other)
+    self.revision <=> other.revision
+  end
+
+  def revision
+    /\w+-(\d+)\.(jpg|gif|png)/ =~ filename ? $1.to_i : -1
+  end
+
+  def ordered_family
+    @ordered_family ||= family_filenames.map do |sibling_path|
+      Mock.for(sibling_path)
+    end.sort
   end
 end
