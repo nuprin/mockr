d21a1f57d65c4b831202ebb382247aa4a2bba2c3
Chase Mathewson <chase@causes.com>
2008-10-15 20:30:00 -0700

    woot...

--- public/javascripts/mockr.js
+++ public/javascripts/mockr.js
@@ -24,7 +24,9 @@ var mockr = function(){
             return area;
         }
         function clear(){
-            $(mockView).html('');
+            $('#mock div.highlight').animate({opacity:0},500,null,function(){
+                $(this).remove();
+            });
             area = null;
             dom = null;
         }
@@ -33,8 +35,8 @@ var mockr = function(){
                 left     : o.x || 0,
                 top      : o.y || 0,
                 opacity  : 0.4,
-                width    : o.w,
-                height   : o.h
+                width    : o.width,
+                height   : o.height
             }).attr({
                 id:o.id
             }).appendTo(mockView)[0];
@@ -67,12 +69,12 @@ var mockr = function(){
             var o = {
                 x : x.start < x.drag ? x.start : x.drag,
                 y : y.start < y.drag ? y.start : y.drag,
-                w : $(dom).width(),
-                h : $(dom).height()
+                width : $(dom).width(),
+                height : $(dom).height()
             };
             x = {};
             y = {};
-            if (o.w < 10 || o.h < 10) {
+            if (o.width < 10 || o.height < 10) {
                 clear();
                 dom = null;
             }
@@ -80,8 +82,8 @@ var mockr = function(){
                 area = o;
                 $('#comment_x').val(o.x||0);
                 $('#comment_y').val(o.y||0);
-                $('#comment_width').val(o.w||0);
-                $('#comment_height').val(o.h||0);
+                $('#comment_width').val(o.width||0);
+                $('#comment_height').val(o.height||0);
             }
         }
 
@@ -98,10 +100,18 @@ var mockr = function(){
         threadView = document.getElementById("comments_list");
         highlight.initialize();
         
-        $("#comments_list li[box]").mouseover(function(){
+        $("#comments_list li[box]").click(function(){
             var box = $(this).attr('box').split('_');
             var id  = $(this).attr('id');
-            highlight.create(box[0],box[1],box[2],box[3],id);
+            highlight.clear();
+            var high = highlight.create({
+                x: box[0],
+                y: box[1],
+                width: box[2],
+                height: box[3],
+                id: id
+            });
+            $(high).css({opacity:0}).animate({opacity:0.7},200).animate({opacity:0.4},200);
         });
         
         $("#feature_list").change(function(event) {
