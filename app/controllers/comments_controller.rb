7e831ad19225205e3688845e62e53b35da2b8a5f
Kristjan <kristjan@causes.com>
2008-10-15 19:40:07 -0700

    Remember who you are

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     params[:comment].delete(:author_id) if params[:comment][:author_id].to_i == 0
     begin
       Comment.create!(params[:comment])
+      session[:user_id] = params[:comment][:author_id].to_i
     rescue ActiveRecord::RecordInvalid => bang
       flash[:notice] = bang.message
     end
