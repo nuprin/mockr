d36f3b01a36ef05f9ff5a3ecdad0057d3eed4963
Chrisling <chris@bananawifi.corp.causes.com>
2008-10-15 18:00:27 -0700

    Better comment validation.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create!(params[:comment])
-    redirect_to mock_url(comment.mock)
+    begin
+      Comment.create!(params[:comment])
+    rescue ActiveRecord::RecordInvalid
+      flash[:notice] = "Please include a comment."
+    end
+    redirect_to :back
   end
 
 end
