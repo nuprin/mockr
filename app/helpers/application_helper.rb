643fffee2f4202392dc1eb39421d4206daab4275
Chris <chris@causes.com>
2008-12-10 10:08:41 -0800

    Preserve line breaks in comment text.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def nl2br(text)
+    text.gsub(/\r\n?/, "\n").gsub(/\n/, '<br />')
+  end
 end
