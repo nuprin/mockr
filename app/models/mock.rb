385e0a13724c7b0a6ddf98f4839277e4d24d8f89
Chris <chris@causes.com>
2009-05-01 19:11:59 -0700

    Get links with spaces working in emails.

--- app/models/mock.rb
+++ app/models/mock.rb
@@ -28,6 +28,16 @@ class Mock < ActiveRecord::Base
     "#{MOCK_PATH}/#{self.path}"
   end
   
+  # url and image_url are hacks until I can figure out how to get normal
+  # routes working in rich text emails.
+  def url
+    "http://mockr/#{URI.encode(self.path)}"
+  end
+
+  def image_url
+    "http://mockr/images/mocks/#{URI.encode(self.path)}"
+  end
+
   def self.for(path)
     full_path = "#{MOCK_PATH}/#{path}"
     unless File.exist?(full_path)
