09861a94a7c3ffc70e89fd27d5f6a539fec9d8ce
Chris Chan <chris@causes.com>
2010-03-22 09:42:14 -0700

    Remove introductory comments.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  helper :all
 
   before_filter :require_authentication
   
