50c61c595fe03c40f47dcbaf78131d918626c56d
Chris Chan <chris@causes.com>
2010-03-23 01:04:16 -0700

    Consider a mock to be updated whenever a comment has been posted to it.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 
   belongs_to :author,
     :class_name => "User"
-  belongs_to :mock
+  belongs_to :mock, :touch => true
   belongs_to :parent,
     :class_name => 'Comment'
   has_many :children,
