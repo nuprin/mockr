35c0c4d48905c6fb93a1c8c8156833296e826dbf
Chris Chan <chris@causes.com>
2010-04-01 10:04:37 -0700

    Remove logging.

--- public/javascripts/plugins/jquery.shortkeys.js
+++ public/javascripts/plugins/jquery.shortkeys.js
@@ -35,7 +35,6 @@ jQuery.fn.shortkeys = jQuery.fn.keys = function (obj, settings) {
 		return inp.toUpperCase().charCodeAt(0);
 	};	
 	this.keyAdd = function(keyCode) {
-	  console.log("Pushing")
 		this.keysDown.push(keyCode);
 		this.keysDown.sort();
 	};
